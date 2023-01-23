19bc45628a7d32fccd609847ac030db75a35d72b
blankf1sh <123241176+blankf1sh@users.noreply.github.com>
2023-01-23 16:36:08 +0000

    setting up for damn-vunerable-defi-v3

---
+++ test/Unstoppable.t.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+contract Unstoppable is Test {
+
+	function setUp() public {
+
+	}
+
+	function testExploit() public {
+
+	}
+
+	function testExploitSuccessful() public {
+
+	}
+}
