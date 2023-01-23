1bdc649c204b28d6ba9bc022cf5190c83ecbce1f
blankf1sh <123241176+blankf1sh@users.noreply.github.com>
2023-01-23 16:43:40 +0000

    clearing

--- test/Unstoppable.t.sol
+++ test/Unstoppable.t.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
+import "lib/damn-vulnerable-defi/contracts/DamnValuableToken.sol";
+import "lib/damn-vulnerable-defi/contracts/unstoppable/UnstoppableVault.sol";
 
 contract Unstoppable is Test {
 
