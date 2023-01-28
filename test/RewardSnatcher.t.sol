6483a60dcea6af7a661d6cafcc4a34786e4531a0
blankf1sh <123241176+blankf1sh@users.noreply.github.com>
2023-01-28 11:58:28 +0000

    using mapping(_keyType => _ValueType) is way nicer than arrays lol

--- test/RewardSnatcher.t.sol
+++ test/RewardSnatcher.t.sol
@@ -2,22 +2,37 @@
 
 pragma solidity ^0.8.0;
 
+import {FlashLoanerPool} from "dvd-v3/the-rewarder/FlashLoanerPool.sol";
 import "forge-std/Test.sol";
 import {Utilities} from "./utils/Utilities.sol";
-import "dvd-v3/the-rewarder/AccountingToken.sol";
-import "dvd-v3/the-rewarder/RewardToken.sol";
-import "dvd-v3/the-rewarder/TheRewarderPool.sol";
+import {AccountingToken} from "dvd-v3/the-rewarder/AccountingToken.sol";
+import {RewardToken} from "dvd-v3/the-rewarder/RewardToken.sol";
+import {TheRewarderPool} from "dvd-v3/the-rewarder/TheRewarderPool.sol";
+import {DamnValuableToken} from "dvd-v3/DamnValuableToken.sol";
+
 
 contract RewardSnatcher is Test {
     uint256 public constant TOKENS_IN_LENDER_POOL = 1_000_000e18;
-    address payable[] public users;
+
+    mapping(string => address payable) internal actors;
+
+    address payable internal attacker;
+
+    DamnValuableToken internal dvt;
+
+    FlashLoanerPool internal flp;
 
     AccountingToken internal at;
     RewardToken internal rt;
     TheRewarderPool internal trp;
 
     function setUp() public {
-
+        Utilities utils = new Utilities();
+        string[6] memory labels = ["Deployer", "Alice", "Bob", "Charlie", "David", "Attacker"];
+        for(uint256 i=0; i<6; ++i) {
+            actors[labels[i]] = utils.getActor(labels[i], 1e18);
+        }
+        attacker = actors["Attacker"];
     }
 
     function test_StoryReward() public view {
@@ -25,16 +40,11 @@ contract RewardSnatcher is Test {
     }
 
     function test_ExploitRewarder() public {
-
+        vm.startPrank(attacker);
+        vm.stopPrank();
     }
 
     function validation() public {
         assertEq(trp.roundNumber(), 3);
-
-        for (uint256 i=0; i< users.length; ++i) {
-            vm.startPrank(users[i]);
-            trp.distributeRewards();
-
-        }
     }
 }
